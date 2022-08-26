2cc2b2cad22d8e395b2e3205f388209b6fa1a3e9
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-26 09:51:44 -0500

    more styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import profilePic from "./images/profile.jpg";
-import Header from './components/Header'
-import Education from './components/Education'
-import Resume from './components/Resume'
+import Header from "./components/Header";
+import Education from "./components/Education";
+import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <body>
-        <Header/>
+        <Header />
         <div class="mainview">
           <h1>Tyler Zeman</h1>
           <h2>Software Developer</h2>
         </div>
         <section id="about-me" class="section sectionStyle">
-          <img id="prfpic"src={profilePic} alt="profile pic"></img>
+          <img id="prfpic" src={profilePic} alt="profile pic"></img>
           <p class="box">
             Hello everyone, my name is Tyler Zeman. I am 23 years old and aspire
             becoming a Full Stack Developer. Before I have taken a 1-month web
@@ -24,10 +24,10 @@ function App() {
             attending this summer through the University of Minnesota.
           </p>
         </section>
-        <Education/>
-        <Resume/>
-        <Projects/>
-        <footer id="footer"class="bottom-bar">
+        <Education />
+        <Resume />
+        <Projects />
+        <footer id="footer" class="bottom-bar">
           <a href="https://github.com/tylerzeman" target="_blank">
             Github
           </a>
