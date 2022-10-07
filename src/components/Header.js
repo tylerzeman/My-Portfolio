38295b083528c8b6002e43063fc899a30262576b
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-10-07 10:46:07 -0500

    added resume to portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 const Header = () => {
   return (
     <header class="main-header">
-          <nav>
-            <a href="#about-me">About</a>
-            <a href="#resume">Resume</a>
-            <a href="#projects">Projects</a>
-            <a href="#footer">Contact</a>
-          </nav>
-        </header>
-  )
-}
+      <nav>
+        <a href="#about-me">About</a>
+        <a href="https://docs.google.com/document/d/1WBq3zM5R4vk9N4lgnAMcHCdFLws4YcpQUoTjzcG7sXo/edit?usp=sharing" target="_blank">Resume</a>
+        <a href="#projects">Projects</a>
+        <a href="#footer">Contact</a>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
