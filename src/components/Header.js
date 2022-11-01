4060457184cfe3343ca6492eb12093e04a9182e3
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-11-01 14:44:46 -0500

    updated contact in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <a href="#about-me">About</a>
         <a href="https://docs.google.com/document/d/1WBq3zM5R4vk9N4lgnAMcHCdFLws4YcpQUoTjzcG7sXo/edit?usp=sharing" target="_blank">Resume</a>
         <a href="#projects">Projects</a>
-        <a href="#footer">Contact</a>
+        <a href="mailto:tylerzeman1999@gmail.com" target="_blank">Contact</a>
       </nav>
     </header>
   );
